20d182395a88a6e24afba4583b2473dff798bc9b
vexper777 <pietropanebianco4@gmail.com>
2026-01-18 18:08:08 +0100

    Update menu-gruppo.js

--- plugins/menu-gruppo.js
+++ plugins/menu-gruppo.js
@@ -1,193 +1,62 @@
+import { performance } from 'perf_hooks';
+import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import '../lib/language.js';
 
-import { promises } from 'fs'
-import { join } from 'path'
-import { xpRange } from '../lib/levelling.js'
-import moment from 'moment-timezone'
-import os from 'os'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const defaultMenu = {
-  before: ``.trimStart(),
-  header: 'ㅤㅤ⋆｡˚『 ╭ \`MENU GRUPPO\` ╯ 』˚｡⋆\n╭',
-  body: '│ ➤『👥』 %cmd\n',
-  footer: '*╰⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*\n',
-  after: ``,                   
-}
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
-let tags = {
-'gruppo': 'Menu Gruppo',
-}
+const handler = async (message, { conn, usedPrefix, command }) => {
 
-  try {
-      let dash = global.dashmenu
-          let m1 = global.dmenut
-      let m2 = global.dmenub
-      let m3 = global.dmenuf
-      let m4 = global.dmenub2
-      let cc = global.cmenut
-      let c1 = global.cmenuh
-      let c2 = global.cmenub
-      let c3 = global.cmenuf
-      let c4 = global.cmenua
-      let lprem = global.lopr
-      let llim = global.lolm
-      let tag = `@${m.sender.split('@')[0]}`
-    let ucpn = `${ucapan()}`
-    let d = new Date(new Date + 3600000)
-    let locale = 'it'
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
+    const menuText = `
+⚡𝑴𝑬𝑵𝑼 𝑨𝑫𝑴𝑰𝑵⚡
+╔═══════════════════╗
 
-    let wib = moment.tz('Europe/Rome').format('HH:mm:ss')
+➥ P/promuovi 👑
+➥ R/retrocevi 🤡
+➥ Warn ☢️
+➥ Listwarn ⏳
+➥ Unwarn ❌
+➥ Delwarn ✅
+➥ Muta 🔇
+➥ Smuta 🔊
+➥ Tag 💬
+➥ Aperto 🔓
+➥ Chiuso 🔒
+➥ Inattivi ⏰
+➥ Admins 👑
+➥ Kick 👋
+➥ SetBye ⛓️
+➥ SetBenvenuto ⛓️
+➥ Link ⚠️
+➥ Linkqr ⚠️
 
-    let mode = global.opts['self'] ? 'Privato' : 'Pubblico'
-    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    let { age, exp, limit, level, role, registered, eris} = global.db.data.users[m.sender]
-    let { min, xp, max } = xpRange(level, global.multiplier)
-    let name = await conn.getName(m.sender)
-    let premium = global.db.data.users[m.sender].premiumTime
-    let prems = `${premium > 0 ? 'Premium': 'Utente comune'}`
-    let platform = os.platform()
+*𝑽𝑬𝑹𝑺𝑰𝑶𝑵𝑬:* *2.0*
 
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
-      return {
-        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
-        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-        prefix: 'customPrefix' in plugin,
-        limit: plugin.limit,
-        premium: plugin.premium,
-        enabled: !plugin.disabled,
-      }
-    })
-    let groups = {}
-    for (let tag in tags) {
-      groups[tag] = []
-      for (let plugin of help)
-        if (plugin.tags && plugin.tags.includes(tag))
-          if (plugin.help) groups[tag].push(plugin)
-          }
-    conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || defaultMenu.before
-    let header = conn.menu.header || defaultMenu.header
-    let body = conn.menu.body || defaultMenu.body
-    let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
-    let _text = [
-      before,
-      ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
-          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%_p' + help)
-                .replace(/%islimit/g, menu.limit ? llim : '')
-                .replace(/%isPremium/g, menu.premium ? lprem : '')
-                .trim()
-            }).join('\n')
-          }),
-          footer
-        ].join('\n')
-      }),
-      after
-    ].join('\n')
-    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-    let replace = {
-      '%': '%',
-      p: _p,
-      muptime: muptime,
-      me: conn.getName(conn.user.jid),
-      npmname: _package.name,
-      npmdesc: _package.description,
-      version: _package.version,
-      exp: exp - min,
-      maxexp: xp,
-      totalexp: exp,
-      xp4levelup: max - exp,
-      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-  dash,m1,m2,m3,m4,cc, c1, c2, c3, c4,lprem,llim,
-  ucpn,platform, wib, mode, _p, eris, age, name, prems, level, limit, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
-      readmore: readMore
-    }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+╚═══════════════════╝
+`.trim();
 
-    await m.react('🕊️')
-    await conn.sendMessage(m.chat, {
-      video: { url: './media/menu/menu3.mp4' },
-      caption: text.trim(),
-      gifPlayback: true,
-      gifAttribution: 2,
-      mimetype: 'video/mp4',
-      ...fake, // Usa il global.fake per il contesto
-      contextInfo: {
-        ...fake.contextInfo, // Mantieni il contesto del fake
-        mentionedJid: [m.sender],
-        forwardedNewsletterMessageInfo: {
-            ...fake.contextInfo.forwardedNewsletterMessageInfo,
-            newsletterName: "ᰔᩚ . ˚ Menu Gruppo ☆˒˒"
-        }
-      }
-    }, { quoted: m })
+    const imagePath = path.join(__dirname, '../media/admin.jpeg');
 
-  } catch (e) {
-    console.error(e)
-    throw `${global.errore}`
-  }
-}
-handler.help = ['menugruppo']
-handler.tags = ['menu']
-handler.command = ['menugruppo', 'menugp', 'menuadmin']
+    await conn.sendMessage(message.chat, {
+        image: { url: imagePath },
+        caption: menuText,
+        buttons: [
+            { buttonId: `${usedPrefix}menu`, buttonText: { displayText: "🏠 Menu Principale" }, type: 1 },
+            { buttonId: `${usedPrefix}menuowner`, buttonText: { displayText: "👑 Menu Owner" }, type: 1 },
+            { buttonId: `${usedPrefix}menusicurezza`, buttonText: { displayText: "🚨 Menu Sicurezza" }, type: 1 },
+            { buttonId: `${usedPrefix}menugruppo`, buttonText: { displayText: "👥 Menu Gruppo" }, type: 1 },
+            { buttonId: `${usedPrefix}menuia`, buttonText: { displayText: "🤖 Menu IA" }, type: 1 }
+        ],
+        viewOnce: true,
+        headerType: 4
+    });
+};
 
-export default handler
+handler.help = ['menuadmin'];
+handler.tags = ['menuadmin'];
+handler.command = /^(menuadmin)$/i;
 
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, ' H ', m, ' M ', s, ' S '].map(v => v.toString().padStart(2, 0)).join('')
-}
-function ucapan() {
-  const time = moment.tz('Europe/Rome').format('HH')
-  let res = "Sveglio così presto? 🥱"
-  if (time >= 4) {
-    res = "Mattina 🌄"
-  }
-  if (time >= 10) {
-    res = "Mattina ☀️"
-  }
-  if (time >= 15) {
-    res = "Pomeriggio 🌇"
-  }
-  if (time >= 18) {
-    res = "Sera 🌙"
-  }
-  return res
-}
+export default handler;
