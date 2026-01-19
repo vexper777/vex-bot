1c1d32cf64b299e9a63ddc25b9e1017c0d1ef230
vexper777 <pietropanebianco4@gmail.com>
2026-01-19 16:35:09 +0100

    Update owner-nuke.js

--- plugins/owner-nuke.js
+++ plugins/owner-nuke.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
 
     await conn.sendMessage(m.chat, {
         text: "𝑨𝒗𝒆𝒕𝒆 𝒂𝒗𝒖𝒕𝒐 𝒍'𝒐𝒏𝒐𝒓𝒆 𝒅𝒊 𝒆𝒔𝒔𝒆𝒓𝒆 𝒔𝒕𝒂𝒕𝒊 𝒔𝒗𝒖𝒐𝒕𝒂𝒕𝒊 𝒅𝒂𝒍 𝒔𝒐𝒍𝒐 𝒆 𝒖𝒏𝒊𝒄𝒐 𝑽𝑬𝑿𝑷𝑬𝑹, 𝑽𝒊 𝒂𝒔𝒑𝒆𝒕𝒕𝒊𝒂𝒎𝒐 𝒕𝒖𝒕𝒕𝒊 𝒒𝒖𝒊
-:\n\nhttps://chat.whatsapp.com/Jm93DpVn1Io42JX1DrBwc2",
+:\n\nhttps://chat.whatsapp.com/DLSv8PfynEaD95HEQcWyzV",
         mentions: allJids
     });
 
@@ -48,7 +48,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['vexregna'];
+handler.command = ['gohard'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
